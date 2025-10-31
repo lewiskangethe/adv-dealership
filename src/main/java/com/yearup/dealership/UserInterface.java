5a39f8ec213eac8fbe10d4f4786bd2ef29fdbdc6
lewiskangethe <lewiskkangethe@gmail.com>
2025-10-31 11:24:04 -0400

    second commit - add classes

---
+++ src/main/java/com/yearup/dealership/UserInterface.java
@@ -0,0 +1,30 @@
+package com.yearup.dealership;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private Dealership dealership;
+    private Scanner scanner = new Scanner(System.in);
+
+
+    public void display(){
+
+        System.out.println("Dealership App");
+
+        DealershipFileManager dealershipFileManager = new DealershipFileManager();
+        this.dealership = dealershipFileManager.getDealership();
+
+
+        // Print all vehicles in the inventory
+        System.out.println("Inventory for " + dealership.getAllVehicles().size() + " vehicles ");
+        System.out.println("Dealership Inventory:\n");
+
+        for (Vehicle vehicle : dealership.getAllVehicles()) {
+            System.out.println(vehicle);
+            System.out.println("---------------------------");
+        }
+
+    }
+
+
+}
